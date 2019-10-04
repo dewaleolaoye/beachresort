b6b53430443aa42c037215b9328e09b908ac7474
dewaleolaoye <adewaleolaoye21@gmail.com>
2019-10-04 22:13:53 +0100

    access data from context

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -4,12 +4,9 @@ import { RoomContext } from '../context';
 export default class FeaturedRooms extends Component {
   static contextType = RoomContext;
   render() {
-    const { greeting, name } = this.context;
+    const { featuredRooms: rooms } = this.context;
+    console.log(rooms);
 
-    return (
-      <div>
-        {greeting} {name} featured rooms
-      </div>
-    );
+    return <div>featured rooms</div>;
   }
 }
